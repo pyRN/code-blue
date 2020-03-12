bedf9345debb4204d6d674a152de7a8c36493afc
Jacob <jacobmayeux@ibm.com>
2020-03-12 12:07:31 -0500

    Styled and added medications to medicationListComponent

--- src/components/MedicationsListComponent.jsx
+++ src/components/MedicationsListComponent.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react'
 
 class MedicationsListComponent extends Component {
-    state = {  }
+    state = { 
+        medications: [
+            "Amiodarone",
+            "Atropine",
+            "Dextrose",
+            "Diltiazem",
+            "Epinepherine",
+            "Lactated Ringer Solution",
+            "Sodium Bicarbonate"
+        ]
+    }
     render() { 
         return (  
             <div className="m-5">
-                <h2>Medications List Component
-                </h2>
+                <div class="input-group mb-3">
+                    <input type="text" className="form-control border-primary" placeholder="Search Medications" aria-label="Search Procedures"/>
+                    <div class="input-group-append">
+                        <button className="text-Primary bg-dark btn btn-outline-primary btn-block" id="basic-addon2">Search</button>
+                    </div>
+                </div>
+                <table className="table table-dark table-striped">
+                    <tbody>
+                        {this.state.medications.map((item) =>
+                            <tr>
+                                <td>{item}</td>
+                            </tr>
+                        )} 
+                    </tbody>
+                </table>
             </div>
         );
     }
 }
- 
+
 export default MedicationsListComponent;
