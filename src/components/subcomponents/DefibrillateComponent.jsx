1843baf1db55d1580d0d7145d2f8aab045d30e29
Jacob <jacobmayeux@ibm.com>
2020-05-14 18:42:16 -0500

    Change to functional component, refactored, changed from seperate card to incorporate into another card

--- src/components/subcomponents/DefibrillateComponent.jsx
+++ src/components/subcomponents/DefibrillateComponent.jsx
@@ -1,37 +1,29 @@
-import React, { Component } from 'react';   
+import React from 'react';   
 
-class DefibrillateComponent extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div className="col-sm card m-1 p-4 border border-dark bg-dark rounded">
-                <div className="card-header bg-secondary">
-                    <h5 className="m-auto">Defibrillate</h5>
-                </div>
-                <div className="card-body">
-                    <div className="input-group border-dark rounded mb-2">
-                        <select className="custom-select" id="joulesGroup">
-                            <option selected>Joules</option>
-                            <option value="1">1-10</option>
-                            <option value="2">15</option>
-                            <option value="3">20</option>
-                            <option value="4">30</option>
-                            <option value="5">50</option>
-                            <option value="6">70</option>
-                            <option value="7">100</option>
-                            <option value="8">120</option>
-                            <option value="9">150</option>
-                            <option value="10">170</option>
-                            <option value="11">200</option>
-                        </select>
-                        <div className="input-group-append">
-                            <button className="text-danger btn btn-warning warning-btn-hover" for="joulesGroup">Shock</button>
-                        </div>
-                    </div>
+function DefibrillateComponent(){
+    return ( 
+        <div className="col-sm">
+            <div className="input-group border-dark rounded mb-2">
+                <select className="custom-select" id="joulesGroup">
+                    <option selected>Joules</option>
+                    <option value="1">1-10</option>
+                    <option value="2">15</option>
+                    <option value="3">20</option>
+                    <option value="4">30</option>
+                    <option value="5">50</option>
+                    <option value="6">70</option>
+                    <option value="7">100</option>
+                    <option value="8">120</option>
+                    <option value="9">150</option>
+                    <option value="10">170</option>
+                    <option value="11">200</option>
+                </select>
+                <div className="input-group-append">
+                    <button className="text-danger btn btn-warning warning-btn-hover" for="joulesGroup">Shock</button>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default DefibrillateComponent;
