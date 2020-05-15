f22760d0c62dad791dc64da638604065e3dbd478
Jacob <jacobmayeux@ibm.com>
2020-05-14 19:15:02 -0500

    Condensed cards for sleeker look

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 //Components
 import CprComponent from './subcomponents/CprComponent'
-import DefibrillateComponent from './subcomponents/DefibrillateComponent'
 import EmergencyMedsComponent from './subcomponents/EmergencyMedsComponent'
 import EmergencyProcComponent from './subcomponents/EmergencyProcComponent'
 import NotesComponent from './subcomponents/NotesComponent'
-import RhythmComponent from './subcomponents/RhythmComponent'
 import VitalsComponent from './subcomponents/VitalsComponent'
 
 const MainComponent = ({addEvent, changeCprTimer, cprTimer}) => {
@@ -15,14 +13,10 @@ const MainComponent = ({addEvent, changeCprTimer, cprTimer}) => {
         <div className="m-4">
             <div className="row">   
                 <CprComponent changeCprTimer={changeCprTimer} cprTimer={cprTimer} addEvent={addEvent}/>
-                <RhythmComponent/>
-            </div>
-            <div className="row">
-                <DefibrillateComponent/>
-                <NotesComponent addEvent={addEvent}/>                    
-            </div>
-            <div className="row">
                 <EmergencyMedsComponent/>
+            </div>
+            <div className="row">
+                <NotesComponent addEvent={addEvent}/> 
                 <EmergencyProcComponent/>   
                 <VitalsComponent/>
             </div>  
