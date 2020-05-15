f2489a6ecfa5c3c1485ccfe7dbb0d973a1bc8e6e
Jacob <jacobmayeux@ibm.com>
2020-05-14 20:39:25 -0500

    Fixed values and added logging functionality

--- src/components/subcomponents/RhythmComponent.jsx
+++ src/components/subcomponents/RhythmComponent.jsx
@@ -1,28 +1,38 @@
 import React from 'react';   
 
-function RhythmComponent(){
+function RhythmComponent({addEvent}){
+    function handleAddNote(e){
+        e.preventDefault();
+        let sInputValue = document.getElementById("rhythmGroup").value
+
+        if(sInputValue === "0") return
+        
+        addEvent('Heart Rhythm', sInputValue)
+        document.getElementById("rhythmGroup").value = 0
+    }
+
     return ( 
-        <div className="col-sm">
-            <div className="input-group border-dark rounded mb-2">
+        <div>
+            <div className="input-group border-dark rounded">
                 <select className="custom-select" id="rhythmGroup">
-                    <option selected>Heart Rhythm</option>
-                    <option value="1">Asystole</option>
-                    <option value="2">A-Fib</option>
-                    <option value="3">A-Flutter</option>
-                    <option value="4">Complete Heart Block</option>
-                    <option value="5">Mono V-Tach</option>
-                    <option value="6">PEA</option>
-                    <option value="7">Poly V-Tach</option>
-                    <option value="8">Pulseless V-Tach</option>
-                    <option value="9">SVT</option>
-                    <option value="10">Torsades</option>
-                    <option value="11">V-Fib</option>
-                    <option value="12">1st&#176; AV Block</option>
-                    <option value="13">2nd&#176; AV Block Type-1</option>
-                    <option value="14">2nd&#176; AV Block Type-2</option>
+                    <option selected value="0">Heart Rhythm</option>
+                    <option value="Asystole">Asystole</option>
+                    <option value="A-Fib">A-Fib</option>
+                    <option value="Flutter">A-Flutter</option>
+                    <option value="Complete Heart Block">Complete Heart Block</option>
+                    <option value="Mono V-Tach">Mono V-Tach</option>
+                    <option value="PEA">PEA</option>
+                    <option value="Poly V-Tach">Poly V-Tach</option>
+                    <option value="Pulseless V-Tach">Pulseless V-Tach</option>
+                    <option value="SVT">SVT</option>
+                    <option value="Torsades">Torsades</option>
+                    <option value="V-Fib">V-Fib</option>
+                    <option value="1st&#176; AV Block">1st&#176; AV Block</option>
+                    <option value="2nd&#176; AV Block Type-1">2nd&#176; AV Block Type-1</option>
+                    <option value="2nd&#176; AV Block Type-2">2nd&#176; AV Block Type-2</option>
                 </select>
                 <div className="input-group-append">
-                    <button className="text-light btn btn-danger start-stop-btn-hover" for="rhythmGroup">Add</button>
+                    <button className="text-light btn btn-danger start-stop-btn-hover" for="rhythmGroup" onClick={handleAddNote}>Add</button>
                 </div>
             </div>
         </div>
