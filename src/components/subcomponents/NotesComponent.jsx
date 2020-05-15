5af9816bcb8108bc8882996e896a6ce2b9cba553
Jacob <jacobmayeux@ibm.com>
2020-05-15 11:16:22 -0500

    Added card styling, moved to different area

--- src/components/subcomponents/NotesComponent.jsx
+++ src/components/subcomponents/NotesComponent.jsx
@@ -13,10 +13,19 @@ function NotesComponent({addEvent}){
     }
 
     return ( 
-        <div className="input-group mb-3">
-            <input type="text" id="notesInput" className="form-control" placeholder="Ad Hoc Note"></input>
-            <div className="input-group-append">
-                <button className="text-primary btn btn-background btn-background-hover" onClick={handleAddNote}>Add</button>
+        <div>
+            <div className="card-header bg-secondary">
+                <h5 className="m-auto">Notes</h5>
+            </div>
+            <div className="card-body">
+                <div>
+                    <div className="input-group mb-3">
+                        <input type="text" id="notesInput" className="form-control" placeholder="Ad Hoc Note"></input>
+                        <div className="input-group-append">
+                            <button className="text-primary btn btn-background btn-background-hover" onClick={handleAddNote}>Add</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
