f9c4a5d940b8a21ebf4bc1a0c59ffcd431c13f43
Jacob <jacobmayeux@ibm.com>
2020-05-15 10:56:02 -0500

    Changed to functional component, added logging functionality, changed from individual buttons to select bar

--- src/components/subcomponents/EmergencyProcComponent.jsx
+++ src/components/subcomponents/EmergencyProcComponent.jsx
@@ -1,23 +1,61 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class EmergencyProcComponent extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div className="col-sm card m-1 p-4 border border-dark bg-dark rounded">
-                <div className="card-header bg-secondary">
-                    <h5 className="m-auto">Emergency Procedures</h5>
-                </div>
-                <div className="card-body">
-                    <button className="text-Primary btn btn-outline-primary btn-block m-1 btn-background">Intubate</button>
-                    <button className="text-Primary btn btn-outline-primary btn-block m-1 btn-background">I.V. Access</button>
-                    <button className="text-Primary btn btn-outline-primary btn-block m-1 btn-background">Central Line</button>
-                    <button className="text-Primary btn btn-outline-primary btn-block m-1 btn-background">Arterial Line</button>
-                    <button className="text-Primary btn btn-outline-primary btn-block m-1 btn-background">Chest Tube</button>
+function EmergencyProcComponent({addEvent}){
+    const handleOnClick = (e) =>{
+        e.preventDefault();
+        let sInputValue = document.getElementById("emergencyProcsGroup").value
+
+        if(sInputValue === "0") return
+
+        switch(sInputValue){
+            case "artLineProc":
+                addEvent("Procedure", "Arterial Line")
+                break
+            case "centLineProc":
+                addEvent("Procedure", "Central Line")
+                break
+            case "chestTubeProc":
+                addEvent("Procedure", "Chest Tube")
+                break
+            case "intubateProc":
+                addEvent("Procedure", "Intubate")
+                break
+            case "ivProc":
+                addEvent("Procedure", "IV Access")
+                break
+            default:
+                break
+        }
+
+        document.getElementById("emergencyProcsGroup").value = 0
+    }
+    return ( 
+        <div>
+            <div className="card-header bg-secondary">
+                <h5 className="m-auto">Emergency Procedures</h5>
+            </div>
+            <div className="card-body">
+                <div className="col-sm">
+                    <div className="input-group border-dark rounded">
+                        <select className="custom-select" id="emergencyProcsGroup">
+                            <option value="0">Emergency Procedures</option>
+                            <option value="artLineProc">Arterial Line</option>
+                            <option value="centLineProc">Central Line</option>
+                            <option value="chestTubeProc">Chest Tube</option>
+                            <option value="intubateProc">Intubate</option>
+                            <option value="ivProc">I.V. Access</option>
+                        </select>
+                        <div className="input-group-append">
+                            <button 
+                                className="text-Primary btn btn-outline-primary btn-background" 
+                                for="emergencyProcsGroup" 
+                                onClick={handleOnClick}>Open</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    )
 }
 
 export default EmergencyProcComponent;
