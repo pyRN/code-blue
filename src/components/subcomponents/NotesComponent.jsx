a9e8e6ec616cbbd386992148ff1333ce675baf3d
Jacob <jacobmayeux@ibm.com>
2020-05-14 20:18:29 -0500

    Changed to functional component, cleaned up inputs and methods

--- src/components/subcomponents/NotesComponent.jsx
+++ src/components/subcomponents/NotesComponent.jsx
@@ -1,39 +1,22 @@
 import React from 'react'
 
-const NotesComponent = ({addEvent}) => {
+function NotesComponent({addEvent}){
 
     const handleAddNote = event => {
         event.preventDefault();
         let sInputValue = document.getElementById("notesInput").value
-        let currentTime = new Date();
 
         if(!sInputValue) return
-
-        addEvent({
-            eventTimestamp: `${currentTime.getHours()}:${currentTime.getMinutes()
-            }:${currentTime.getSeconds()}:${currentTime.getMilliseconds()} - ${currentTime.getMonth() + 1}/${currentTime.getDate()}/${currentTime.getFullYear()}`,
-            eventType: "ad hoc note",
-            eventDescription: sInputValue
-        })
-
+        
+        addEvent('ad hoc note', sInputValue)
         document.getElementById("notesInput").value = ''
     }
 
-
     return ( 
-        <div className="col-sm card m-1 p-4 border border-dark bg-dark rounded">
-            <div className="card-header bg-secondary">
-                <h5 className="m-auto">Notes</h5>
-            </div>
-            <div className="card-body">
-                <div className="input-group border-dark rounded mb-2">
-                    <form onSubmit={handleAddNote}>
-                        <input type="text" id="notesInput" className="form-control border-primary" placeholder="Ad Hoc Note" onSubmit={handleAddNote}></input>
-                        <div className="input-group-append">
-                            <button className="text-primary btn btn-background btn-background-hover" onClick={handleAddNote}>Add</button>
-                        </div>
-                    </form>
-                </div>
+        <div className="input-group mb-3">
+            <input type="text" id="notesInput" className="form-control" placeholder="Ad Hoc Note"></input>
+            <div className="input-group-append">
+                <button className="text-primary btn btn-background btn-background-hover" onClick={handleAddNote}>Add</button>
             </div>
         </div>
     );
